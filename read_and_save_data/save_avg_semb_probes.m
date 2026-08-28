db29d3d597fac7ea635da03f16f673daa9d2878d
Rebecca Shannon <rebshan@hpcmp,hpc.mil>
2026-08-28 20:35:23 +0000

    semb avg probes

--- read_and_save_data/save_avg_semb_probes.m
+++ read_and_save_data/save_avg_semb_probes.m
@@ -7,14 +7,25 @@
 tStart = 0;
 tEnd = 239;
 
+DODHOME = '/p/work1/rebshan/';
+caseDir = 'sembcomp.NARWHAL/postProcessing';
+
+cd(strcat(DODHOME,caseDir));
+
 
 for probe = 1:3
-    for time = tStart:tEnd
 
-        fName = strcat('probe',num2str(probe),'/probe',num2str(probe),'_allPoints_',num2str(time),'.csv');
+    cd(strcat('probe',num2str(probe)))
+    numSteps = length(dir(fullfile('.','*.csv')));
+
+    for time = tStart:numSteps-1
+        fName = strcat('probe',num2str(probe),'_allPoints_',num2str(time),'.csv');
         M = readmatrix(fName);
         pres = mean(M(:,2));
-        vofFoam_semb(probe).p(time + 1) = pres;
+        tmp(probe).p(time + 1) = pres;
     end
+
+    cd ..
 end
 
+tait = tmp;
